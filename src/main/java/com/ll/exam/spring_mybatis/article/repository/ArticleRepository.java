693e8229892358612720d898f90ae9cf6e5b1438
kimdonghyeon3 <sh2115521@naver.com>
2022-09-22 12:05:12 +0900

    chore : 오류

--- src/main/java/com/ll/exam/spring_mybatis/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/spring_mybatis/article/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package com.ll.exam.spring_mybatis.article.repository;
 
 import com.ll.exam.spring_mybatis.article.dto.Article;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -17,13 +18,13 @@ public interface ArticleRepository {
             """)
     List<Article> getArticles();
 
-    @Select("""
+    @Insert("""
             <script>
             INSERT INTO article
             SET createDate = NOW(),
             modifyDate = NOW(),
-            `subject` = '제목2',
-            content = '내용2';
+            subject = #{subject},
+            content = #{content}
             </script>
             """)
     void write(String subject, String content);
